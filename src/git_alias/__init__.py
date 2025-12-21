023ec2967391d8e459d65cb628d2c79f0eead1b9
Francesco Rolando <ogekuri@gmail.com>
2025-12-21 19:16:24 +0100

    release version: 0.0.15

--- src/git_alias/__init__.py
+++ src/git_alias/__init__.py
@@ -1,5 +1,5 @@
 """Pacchetto principale della CLI git-alias per uvx."""
 
-__version__ = "0.0.14"
+__version__ = "0.0.15"
 
 from .core import main  # noqa: F401
--- venv.sh
+++ venv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# VERSION: # VERSION: 0.0.14
+# VERSION: # VERSION: 0.0.15
 # AUTHORS: Ogekuri
 
 cd -- "$(dirname "$0")/"
