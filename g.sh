11715db0fa6fb3c5eabf0e4991020a51cb34c3c4
Francesco Rolando <ogekuri@gmail.com>
2026-01-01 15:08:42 +0100

    release version: 0.0.19

--- g.sh
+++ g.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
-# VERSION: 0.0.18
+# VERSION: 0.0.19
 # AUTHORS: Ogekuri
 
 # 1. Ottieni il percorso assoluto completo del file (risolvendo i link simbolici)
--- src/git_alias/__init__.py
+++ src/git_alias/__init__.py
@@ -1,6 +1,6 @@
 """Pacchetto principale della CLI git-alias per uvx."""
 
-__version__ = "0.0.18"
+__version__ = "0.0.19"
 
 from .core import main  # noqa: F401
 
